16253ca39a043bd13ac3c43ff3355f9f0e41816d
luoyecong <luoyec@talkmoney.cn>
2017-12-06 13:01:58 +0800

    <A> HMR模块热更新

--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe() {
-    console.error('I get called from 2333 print.js!');
+    // console.error('I get called from 2333 print.js!');
+    console.log('Updating print.js...0011');
 }
