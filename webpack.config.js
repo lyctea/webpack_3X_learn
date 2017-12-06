e67188b4e42a687a6952f38004b2b9d9515d86ff
luoyecong <luoyec@talkmoney.cn>
2017-12-06 17:56:24 +0800

    <A> 创建并发布npm包

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'webpack-numbers.js'
+        filename: 'webpack-numbers.js',
+        library: 'webpackNumbers',
+        libraryTarget: 'umd'
     },
     externals: {
         lodash: {
